c8e8d108fc708588467b46f422a33e77f521cd65
dohlee <dohlee.bioinfo@gmail.com>
2023-04-19 17:40:18 +0900

    add basic cli tests for all subcommands

---
+++ tests/lpmd-cli.rs
@@ -0,0 +1,34 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+
+#[test]
+fn simple_run() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("metheor")?;
+
+    cmd.arg("lpmd")
+        .arg("-i")
+        .arg("tests/test1.bam")
+        .arg("-o")
+        .arg("tests/test1.lpmd.tsv")
+        .assert()
+        .success();
+
+    Ok(())
+}
+#[test]
+fn input_bam_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("metheor")?;
+
+    cmd.arg("lpmd")
+        .arg("-i")
+        .arg("tests/no_such.bam")
+        .arg("-o")
+        .arg("tests/test1.lpmd.tsv")
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains("file not found"))
+        .stderr(predicate::str::contains("no_such.bam"));
+
+    Ok(())
+}
