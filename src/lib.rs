dabee6424aba80ecbfb55d77d79aeffaf28e7414
dohlee <dohlee.bioinfo@gmail.com>
2023-05-15 11:25:12 +0900

    Bump version: 0.1.6 → 0.1.7

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ pub mod readutil;
 #[derive(Parser)]
 #[clap(name = "metheor")]
 #[clap(about = "Summarizes the heterogeneity of DNA methylation states using BAM files.")]
-#[clap(version = "0.1.6")]
+#[clap(version = "0.1.7")]
 #[clap(
     author = "Dohoon Lee. <dohlee.bioinfo@gmail.com>\nBonil Koo. <bikoo95@snu.ac.kr>\nJeewon Yang. <jwyang.ai@gmail.com>"
 )]
